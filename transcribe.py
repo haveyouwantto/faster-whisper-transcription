06cd6730ad81ecb516b76c81794ee94d9967902c
haveyouwantto <haveyouwantto@gmail.com>
2023-03-18 00:46:17 +0800

    fix overwrite again

--- transcribe.py
+++ transcribe.py
@@ -55,75 +55,71 @@ def format_ass_time(seconds):
 # should be appended to an existing file instead of overwriting it.
 def gen_subtitles(segments, outname, append=False):
     # Open the output file for writing or appending
-    filename = outname + '.ass'
-    if args.force_overwrite or (not os.path.exists(filename) or append):
-        t = open(filename, 'a' if append else 'w', encoding='utf-8')
+    t = open(outname, 'a' if append else 'w', encoding='utf-8')
 
-        # Write the ASS subtitle file header
-        if not append:
-            t.write(ass_header)
+    # Write the ASS subtitle file header
+    if not append:
+        t.write(ass_header)
 
-        # Set the style based on whether the subtitle is being appended or not
-        style = 'Small' if append else 'Default'
+    # Set the style based on whether the subtitle is being appended or not
+    style = 'Small' if append else 'Default'
 
-        try:
-            # Iterate through each segment and generate the corresponding subtitle lines
-            for i, segment in enumerate(segments):
-                # Format the start and end times for the subtitle line
-                start_time = format_srt_time(segment.start)
-                end_time = format_srt_time(segment.end)
+    try:
+        # Iterate through each segment and generate the corresponding subtitle lines
+        for i, segment in enumerate(segments):
+            # Format the start and end times for the subtitle line
+            start_time = format_srt_time(segment.start)
+            end_time = format_srt_time(segment.end)
 
-                # Replace any newline characters in the segment text with a space
-                text = segment.text.replace('\n', ' ')
+            # Replace any newline characters in the segment text with a space
+            text = segment.text.replace('\n', ' ')
 
-                # Create the subtitle line with the index, start and end times, and segment text
-                line = f"{i+1}\n{start_time} --> {end_time}\n{text}"
+            # Create the subtitle line with the index, start and end times, and segment text
+            line = f"{i+1}\n{start_time} --> {end_time}\n{text}"
 
-                # Print the segment information to the console
-                print("[%s -> %s] %s" % (start_time, end_time, segment.text))
+            # Print the segment information to the console
+            print("[%s -> %s] %s" % (start_time, end_time, segment.text))
 
-                # Write the subtitle line to the output file
-                # f.write(line+'\n\n')
+            # Write the subtitle line to the output file
+            # f.write(line+'\n\n')
 
-                # if word_ts:
-                # Iterate through each word in the segment and generate the corresponding subtitle lines
-                pos = 0
-                last_end = segment.start
-                for word in segment.words:
-                    st = word.start
-                    ed = word.end
-                    start_time = format_ass_time(st)
-                    end_time = format_ass_time(ed)
+            # if word_ts:
+            # Iterate through each word in the segment and generate the corresponding subtitle lines
+            pos = 0
+            last_end = segment.start
+            for word in segment.words:
+                st = word.start
+                ed = word.end
+                start_time = format_ass_time(st)
+                end_time = format_ass_time(ed)
 
-                    # If the current word starts after the end of the last word, generate a subtitle line for the gap
-                    if st > last_end:
-                        line = f"Dialogue: 0,{format_ass_time(last_end)},{format_ass_time(st)},{style},,0,0,0,,{segment.text}"
-                        t.write(line+'\n')
-
-                    # Insert the formatting codes for the word in the segment text
-                    wordlen = len(word.word)
-                    text = list(segment.text)
-                    text.insert(pos+wordlen, '{\c}')
-                    text.insert(pos, '{\c&H9628E6&}')
-
-                    # Create the subtitle line for the word
-                    line = f"Dialogue: 0,{start_time},{end_time},{style},,0,0,0,,{''.join(text)}"
+                # If the current word starts after the end of the last word, generate a subtitle line for the gap
+                if st > last_end:
+                    line = f"Dialogue: 0,{format_ass_time(last_end)},{format_ass_time(st)},{style},,0,0,0,,{segment.text}"
                     t.write(line+'\n')
-                    pos += wordlen
 
-                    # Set the end time of the last word to the current word's end time
-                    last_end = ed
-        
-        # Handling of https://github.com/guillaumekln/faster-whisper/issues/50
-        except IndexError as e:
-            print(e)
-        
-        # Close the output file
-        # f.close()
-        # if word_ts:
-        t.close()
-    else:
-        print(f"Skipping {filename} (file already exists). Use --force-overwrite to overwrite existing files.")
+                # Insert the formatting codes for the word in the segment text
+                wordlen = len(word.word)
+                text = list(segment.text)
+                text.insert(pos+wordlen, '{\c}')
+                text.insert(pos, '{\c&H9628E6&}')
+
+                # Create the subtitle line for the word
+                line = f"Dialogue: 0,{start_time},{end_time},{style},,0,0,0,,{''.join(text)}"
+                t.write(line+'\n')
+                pos += wordlen
+
+                # Set the end time of the last word to the current word's end time
+                last_end = ed
+    
+    # Handling of https://github.com/guillaumekln/faster-whisper/issues/50
+    except IndexError as e:
+        print(e)
+    
+    # Close the output file
+    # f.close()
+    # if word_ts:
+    t.close()
 
 # Iterate through each audio file provided in the command line arguments
 for audio_file in args.audio_files:
@@ -138,17 +134,23 @@ for audio_file in args.audio_files:
     print(f"Detected language '{info.language}' with probability {info.language_probability}")
     
     # Generate subtitles file with the same name as the original audio file and the detected language as the extension
-    output_file = f"{name}.{info.language}"
-    gen_subtitles(segments, output_file)
+    output_file = f"{name}.{info.language}.ass"
 
-    # If the detected language is not English, transcribe the audio using translation
-    if not args.no_translate and info.language != 'en':
-        segments, info = model.transcribe(audio_file, beam_size=5, task='translate', word_timestamps=True)
+    if args.force_overwrite or not os.path.exists(output_file):
+        
+        gen_subtitles(segments, output_file)
 
-        # output_file = f"{name}.en.translated"
+        # If the detected language is not English, transcribe the audio using translation
+        if not args.no_translate and info.language != 'en':
+            segments, info = model.transcribe(audio_file, beam_size=5, task='translate', word_timestamps=True)
 
-        # Append English translation
-        gen_subtitles(segments, output_file, append=True)
-    
-    # Print the name of the output subtitle file
-    print(f"Subtitles saved to {output_file}")
+            # output_file = f"{name}.en.translated"
+
+            # Append English translation
+            gen_subtitles(segments, output_file, append=True)
+        
+        # Print the name of the output subtitle file
+        print(f"Subtitles saved to {output_file}")
+
+    else:
+        print(f"Skipping {output_file} (file already exists). Use --force-overwrite to overwrite existing files.")
